5e7da44b527b8db5ef8166bd9fddf4d766b12153
ginpei <ginpei@gmail.com>
2020-11-18 13:25:36 -0800

    first version

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders steps", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Step 0 \/ 6/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
+const totalSteps = 6;
+
 function App(): JSX.Element {
+  const [step, setStep] = useState(0);
+
+  const onBackClick = useCallback(() => {
+    if (step <= 0) {
+      return;
+    }
+
+    setStep(step - 1);
+  }, [step]);
+
+  const onNextClick = useCallback(() => {
+    if (step >= totalSteps) {
+      return;
+    }
+
+    setStep(step + 1);
+  }, [step]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ui-container">
+      <p>
+        Step {step} / {totalSteps}
+        <br />
+        <button onClick={onBackClick}>Back</button>
+        <button onClick={onNextClick}>Next</button>
+      </p>
+      <div
+        className="App-box"
+        data-step-1={step >= 1}
+        data-step-2={step >= 2}
+        data-step-3={step >= 3}
+        data-step-4={step >= 4}
+        data-step-5={step >= 5}
+        data-step-6={step >= 6}
+      >
+        <span className="ui-borderedText">竈</span>
+      </div>
     </div>
   );
 }
